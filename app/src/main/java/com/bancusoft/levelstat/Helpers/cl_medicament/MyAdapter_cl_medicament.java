fca3f25948cb20ca83ee5449200e805397ca61c7
iostream17 <17309940+iostream17@users.noreply.github.com>
2021-03-07 12:46:26 +0200

    change domain

---
+++ app/src/main/java/com/bancusoft/levelstat/Helpers/cl_medicament/MyAdapter_cl_medicament.java
@@ -0,0 +1,28 @@
+package com.bancusoft.levelstat.Helpers.cl_medicament;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.style.ForegroundColorSpan;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.ivbaranov.mli.MaterialLetterIcon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+import com.bancusoft.levelstat.R;
+import com.bancusoft.levelstat.Retrofit.Cl_medicament;
+import com.bancusoft.levelstat.Views.med.DetailActivity_cl_med;
+
+public class MyAdapter_cl_medicament {
+}
