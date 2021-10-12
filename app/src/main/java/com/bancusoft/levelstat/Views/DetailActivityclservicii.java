8b65c01f73d4e19e32f2eabe611229a3f926b8e4
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-10-12 15:26:43 +0300

    Fixed some problems -DetailActivityclservicii

--- app/src/main/java/com/bancusoft/levelstat/Views/DetailActivityclservicii.java
+++ app/src/main/java/com/bancusoft/levelstat/Views/DetailActivityclservicii.java
@@ -7,18 +7,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-
-
-import com.bancusoft.levelstat.Retrofit.Cl_servicii;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.bancusoft.levelstat.Helpers.Utils;
 import com.bancusoft.levelstat.R;
+import com.bancusoft.levelstat.Retrofit.Cl_servicii;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
@@ -55,13 +53,9 @@ public class DetailActivityclservicii extends AppCompatActivity{
             CODUL_TV_CSPM.setText(receivedCl_servicii.getCODUL());
             DENUMIRE_TV_CSPM.setText(receivedCl_servicii.getDENUMIRE());
 
-
-
-
             mCollapsingToolbarLayout.setTitle(receivedCl_servicii.getCODUL());
+            mCollapsingToolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, R.color.white));
 
-            mCollapsingToolbarLayout.setExpandedTitleColor(getResources().
-                    getColor(R.color.white));
         }
 
     }
@@ -83,34 +77,53 @@ public class DetailActivityclservicii extends AppCompatActivity{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_edit_cl_servicii:
-                Utils.sendCl_serviciiToActivity(this,receivedCl_servicii,help_vw.class);
-                finish();
-                return true;
 
+        int id = item.getItemId();
 
-            case R.id.action_edit_en_cl_servicii:
-                Utils.sendCl_serviciiToActivity(this,receivedCl_servicii,help_vw_en.class);
-                finish();
-                return true;
+        if (id==R.id.action_edit_cl_servicii){
+            Utils.sendCl_serviciiToActivity(this,receivedCl_servicii,help_vw.class);
+            finish();
+            return true;
 
-            case R.id.action_edit_ru_cl_servicii:
-                Utils.sendCl_serviciiToActivity(this,receivedCl_servicii,help_vw_ru.class);
-                finish();
-                return true;
-
-            case android.R.id.home:
-                //NavUtils.navigateUpFromSameTask(this);
-                Utils.sendCl_serviciiToActivity(this,receivedCl_servicii,ScientistsActivityclservicii.class);
-                finish();
-                return true;
-
-            case R.id.video3_cl_servicii:
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(com.bancusoft.levelstat.Helpers.Utils.youtube_level_stat ));
-                startActivity(browserIntent);
-                break;
         }
+        else
+
+        if (id==R.id.action_edit_en_cl_servicii){
+            Utils.sendCl_serviciiToActivity(this,receivedCl_servicii,help_vw_en.class);
+            finish();
+            return true;
+
+        }
+        else
+
+
+        if (id==R.id.action_edit_ru_cl_servicii){
+            Utils.sendCl_serviciiToActivity(this,receivedCl_servicii,help_vw_ru.class);
+            finish();
+            return true;
+
+        }
+        else
+
+        if (id == android.R.id.home){
+
+            Intent intent;
+            intent = new Intent(this,ScientistsActivityclservicii.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            finish();
+            startActivity(intent);
+            return true;
+
+        }
+
+        else
+        if (id == R.id.video3_cl_servicii) {
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(com.bancusoft.levelstat.Helpers.Utils.youtube_level_stat));
+            startActivity(browserIntent);
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -152,29 +165,22 @@ public class DetailActivityclservicii extends AppCompatActivity{
 
         Button mBtnShare = findViewById(R.id.btnShare_cspm);
 
-        mBtnShare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String s_CODUL_TV_CSPM = CODUL_TV_CSPM.getText().toString();
-                String s_DENUMIRE_TV_CSPM = DENUMIRE_TV_CSPM.getText().toString();
+        mBtnShare.setOnClickListener(view -> {
+            String s_CODUL_TV_CSPM = CODUL_TV_CSPM.getText().toString();
+            String s_DENUMIRE_TV_CSPM = DENUMIRE_TV_CSPM.getText().toString();
 
 
+            String contentShare = " Codul servicii : " + s_CODUL_TV_CSPM + " - Denumirea codului, \n" +
+                    "  :  " + s_DENUMIRE_TV_CSPM;
 
+            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
 
-                String a = " Codul servicii : " + s_CODUL_TV_CSPM + " - Denumirea codului, \n" +
-                        "  :  " + s_DENUMIRE_TV_CSPM;
+            sharingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Clasificatorul serviciilor în domeniul activităţii economice externe");
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, contentShare);
 
-
-                String contentShare = new String(a);
-
-                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                sharingIntent.setType("text/plain");
-                sharingIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Clasificatorul serviciilor în domeniul activităţii economice externe");
-                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, contentShare);
-
-                startActivity(Intent.createChooser(sharingIntent, "Share text via"));
-            }
+            startActivity(Intent.createChooser(sharingIntent, "Share text via"));
         });
 
 
