72338a9e25ea3f7b23acd930f0bccc245dc00331
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-10-17 12:06:26 +0300

    helpen -- Solved problems

--- app/src/main/java/com/bancusoft/levelstat/Views/helpen.java
+++ app/src/main/java/com/bancusoft/levelstat/Views/helpen.java
@@ -34,8 +34,6 @@ public class helpen extends BaseActivity {
 
     @Override
     public void onBackPressed() {
-//        super.onBackPressed();
-//        this.finish();
 
         Utils.showInfoDialog_help_en(this,
                 "Attention!", "Are you sure you want to exit? We have translate help in romanian and russian.");
@@ -47,8 +45,6 @@ public class helpen extends BaseActivity {
      */
 
     public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.help, menu);
 
         getMenuInflater().inflate(R.menu.help, menu);
         return true;
@@ -60,34 +56,62 @@ public class helpen extends BaseActivity {
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.rolang:
-                Utils.sendScientistToActivity(this, receivedScientist, help.class);
-                finish();
-                return true;
 
-            case R.id.enlang:
-                Utils.sendScientistToActivity(this, receivedScientist, helpen.class);
-                finish();
-                return true;
 
-            case R.id.rulang:
-                Utils.sendScientistToActivity(this,receivedScientist,helpru.class);
-                finish();
-                return true;
 
-            case R.id.link1:
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(com.bancusoft.levelstat.Helpers.Utils.youtube_level_stat ));
-                startActivity(browserIntent);
-                break;
+        int id = item.getItemId();
 
-//            case android.R.id.home:
-//
-//                Utils.sendScientistToActivity(this,receivedScientist,ScientistsActivity.class);
-//                finish();
-//                return true;
+        if (id==R.id.rolang){
+            Utils.sendScientistToActivity(this,receivedScientist,help.class);
+            finish();
+            return true;
         }
+        else
+
+        if (id==R.id.enlang){
+            Utils.sendScientistToActivity(this,receivedScientist,helpen.class);
+            finish();
+            return true;
+
+        }
+
+        else
+        if (id==R.id.rulang){
+            Utils.sendScientistToActivity(this,receivedScientist,helpru.class);
+            finish();
+            return true;
+        }
+
+
+        else
+
+        if (id == android.R.id.home){
+
+            Intent intent;
+            intent = new Intent(this, ScientistsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            finish();
+            startActivity(intent);
+            return true;
+
+        }
+
+        else
+        if (id == R.id.link1) {
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(com.bancusoft.levelstat.Helpers.Utils.youtube_level_stat));
+
+            startActivity(browserIntent);
+
+
+        }
+
+
         return super.onOptionsItemSelected(item);
 
+    }
+
+    public void setReceivedScientist(Scientist receivedScientist) {
+        this.receivedScientist = receivedScientist;
     }
 }
