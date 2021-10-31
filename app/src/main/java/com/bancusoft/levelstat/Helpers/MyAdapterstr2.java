0c8e8d3414d4470e5a117aff46bcd617ab186787
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-10-31 19:07:56 +0200

    MyAdapter_all -- Solved problems

--- app/src/main/java/com/bancusoft/levelstat/Helpers/MyAdapterstr2.java
+++ app/src/main/java/com/bancusoft/levelstat/Helpers/MyAdapterstr2.java
@@ -22,11 +22,10 @@ import com.github.ivbaranov.mli.MaterialLetterIcon;
 
 public class MyAdapterstr2 extends RecyclerView.Adapter<MyAdapterstr2.ViewHolder>{
 
-    private Context c;
-    private final TypedValue mTypedValue = new TypedValue();
-    private int mBackground;
-    private int[] mMaterialColors;
-    private List<Scientist> scientists;
+    private final Context c;
+    private final int mBackground;
+    private final int[] mMaterialColors;
+    private final List<Scientist> scientists;
 
     public String searchString = "";
 
@@ -35,11 +34,21 @@ public class MyAdapterstr2 extends RecyclerView.Adapter<MyAdapterstr2.ViewHolder
      * 1. Hold all the widgets which will be recycled and reference them.
      * 2. Implement click event.
      */
-    public class ViewHolder extends RecyclerView.ViewHolder implements
+    public static class ViewHolder extends RecyclerView.ViewHolder implements
             View.OnClickListener {
-        private TextView nameTxt, starTxt, galaxyTxt, departTxt, sectiaTxt, serviciuTxt, phoneTxt, descriptionTxt, formnamneTxt,
-                phonemobileTxt,emailTxt,noticeTxt;
-        private MaterialLetterIcon mIcon;
+        private final TextView nameTxt;
+        private final TextView starTxt;
+        private final TextView galaxyTxt;
+        private final TextView departTxt;
+        private final TextView sectiaTxt;
+        private final TextView serviciuTxt;
+        private final TextView phoneTxt;
+        private final TextView descriptionTxt;
+        private final TextView formnamneTxt;
+        private final TextView phonemobileTxt;
+        private final TextView emailTxt;
+        private final TextView noticeTxt;
+        private final MaterialLetterIcon mIcon;
         private MyAdapterstr2.ItemClickListener itemClickListener;
         /**
          * We reference our widgets
@@ -79,6 +88,7 @@ public class MyAdapterstr2 extends RecyclerView.Adapter<MyAdapterstr2.ViewHolder
     public MyAdapterstr2(Context mContext, ArrayList<Scientist> scientists) {
         this.c = mContext;
         this.scientists = scientists;
+        TypedValue mTypedValue = new TypedValue();
         c.getTheme().resolveAttribute(R.attr.selectableItemBackground, mTypedValue, true);
         mMaterialColors = c.getResources().getIntArray(R.array.colors);
         mBackground = mTypedValue.resourceId;
@@ -92,8 +102,7 @@ public class MyAdapterstr2 extends RecyclerView.Adapter<MyAdapterstr2.ViewHolder
     public MyAdapterstr2.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(c).inflate(R.layout.model_str, parent, false);
         view.setBackgroundResource(mBackground);
-        MyAdapterstr2.ViewHolder vh = new MyAdapterstr2.ViewHolder(view);
-        return vh;
+        return new ViewHolder(view);
     }
     /**
      * Our onBindViewHolder method
--- app/src/main/java/com/bancusoft/levelstat/Helpers/MyAdapterstr3.java
+++ app/src/main/java/com/bancusoft/levelstat/Helpers/MyAdapterstr3.java
@@ -21,11 +21,10 @@ import com.github.ivbaranov.mli.MaterialLetterIcon;
 
 public class MyAdapterstr3  extends RecyclerView.Adapter<MyAdapterstr3.ViewHolder>{
 
-    private Context c;
-    private final TypedValue mTypedValue = new TypedValue();
-    private int mBackground;
-    private int[] mMaterialColors;
-    private List<Scientist> scientists;
+    private final Context c;
+    private final int mBackground;
+    private final int[] mMaterialColors;
+    private final List<Scientist> scientists;
 
     public String searchString = "";
 
@@ -34,11 +33,21 @@ public class MyAdapterstr3  extends RecyclerView.Adapter<MyAdapterstr3.ViewHolde
      * 1. Hold all the widgets which will be recycled and reference them.
      * 2. Implement click event.
      */
-    public class ViewHolder extends RecyclerView.ViewHolder implements
+    public static class ViewHolder extends RecyclerView.ViewHolder implements
             View.OnClickListener {
-        private TextView nameTxt, starTxt, galaxyTxt, departTxt, sectiaTxt, serviciuTxt, phoneTxt, descriptionTxt, formnamneTxt,
-                phonemobileTxt,emailTxt,noticeTxt;
-        private MaterialLetterIcon mIcon;
+        private final TextView nameTxt;
+        private final TextView starTxt;
+        private final TextView galaxyTxt;
+        private final TextView departTxt;
+        private final TextView sectiaTxt;
+        private final TextView serviciuTxt;
+        private final TextView phoneTxt;
+        private final TextView descriptionTxt;
+        private final TextView formnamneTxt;
+        private final TextView phonemobileTxt;
+        private final TextView emailTxt;
+        private final TextView noticeTxt;
+        private final MaterialLetterIcon mIcon;
         private MyAdapterstr3.ItemClickListener itemClickListener;
         /**
          * We reference our widgets
@@ -78,6 +87,7 @@ public class MyAdapterstr3  extends RecyclerView.Adapter<MyAdapterstr3.ViewHolde
     public MyAdapterstr3(Context mContext, ArrayList<Scientist> scientists) {
         this.c = mContext;
         this.scientists = scientists;
+        TypedValue mTypedValue = new TypedValue();
         c.getTheme().resolveAttribute(R.attr.selectableItemBackground, mTypedValue, true);
         mMaterialColors = c.getResources().getIntArray(R.array.colors);
         mBackground = mTypedValue.resourceId;
@@ -91,8 +101,7 @@ public class MyAdapterstr3  extends RecyclerView.Adapter<MyAdapterstr3.ViewHolde
     public MyAdapterstr3.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(c).inflate(R.layout.model_str, parent, false);
         view.setBackgroundResource(mBackground);
-        MyAdapterstr3.ViewHolder vh = new MyAdapterstr3.ViewHolder(view);
-        return vh;
+        return new ViewHolder(view);
     }
     /**
      * Our onBindViewHolder method
