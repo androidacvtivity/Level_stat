29b4a6650c147a89de4fbee8eeccb29cf5c7ed3c
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-10-31 18:08:35 +0200

    MyAdapter_mun_chis_cer_gospodarii-- Solved problems

--- app/src/main/java/com/bancusoft/levelstat/Helpers/structbns/MyAdapter_mun_chis_cer_gospodarii.java
+++ app/src/main/java/com/bancusoft/levelstat/Helpers/structbns/MyAdapter_mun_chis_cer_gospodarii.java
@@ -25,11 +25,10 @@ import com.bancusoft.levelstat.Views.structbns.DetailActivity_mun_cer_gospodarii
 import com.github.ivbaranov.mli.MaterialLetterIcon;
 public class MyAdapter_mun_chis_cer_gospodarii extends RecyclerView.Adapter<MyAdapter_mun_chis_cer_gospodarii.ViewHolder>{
 
-    private Context c;
-    private final TypedValue mTypedValue = new TypedValue();
-    private int mBackground;
-    private int[] mMaterialColors;
-    private List<Scientist> scientists;
+    private final Context c;
+    private final int mBackground;
+    private final int[] mMaterialColors;
+    private final List<Scientist> scientists;
 
     public String searchString = "";
 
@@ -38,11 +37,21 @@ public class MyAdapter_mun_chis_cer_gospodarii extends RecyclerView.Adapter<MyAd
      * 1. Hold all the widgets which will be recycled and reference them.
      * 2. Implement click event.
      */
-    public class ViewHolder extends RecyclerView.ViewHolder implements
+    public static class ViewHolder extends RecyclerView.ViewHolder implements
             View.OnClickListener {
-        private TextView nameTxt, starTxt, galaxyTxt, departTxt, sectiaTxt, serviciuTxt, phoneTxt, descriptionTxt, formnamneTxt,
-                phonemobileTxt,emailTxt,noticeTxt;
-        private MaterialLetterIcon mIcon;
+        private final TextView nameTxt;
+        private final TextView starTxt;
+        private final TextView galaxyTxt;
+        private final TextView departTxt;
+        private final TextView sectiaTxt;
+        private final TextView serviciuTxt;
+        private final TextView phoneTxt;
+        private final TextView descriptionTxt;
+        private final TextView formnamneTxt;
+        private final TextView phonemobileTxt;
+        private final TextView emailTxt;
+        private final TextView noticeTxt;
+        private final MaterialLetterIcon mIcon;
         private MyAdapter_mun_chis_cer_gospodarii.ItemClickListener itemClickListener;
         /**
          * We reference our widgets
@@ -82,6 +91,7 @@ public class MyAdapter_mun_chis_cer_gospodarii extends RecyclerView.Adapter<MyAd
     public MyAdapter_mun_chis_cer_gospodarii(Context mContext, ArrayList<Scientist> scientists) {
         this.c = mContext;
         this.scientists = scientists;
+        TypedValue mTypedValue = new TypedValue();
         c.getTheme().resolveAttribute(R.attr.selectableItemBackground, mTypedValue, true);
         mMaterialColors = c.getResources().getIntArray(R.array.colors);
         mBackground = mTypedValue.resourceId;
@@ -95,8 +105,7 @@ public class MyAdapter_mun_chis_cer_gospodarii extends RecyclerView.Adapter<MyAd
     public MyAdapter_mun_chis_cer_gospodarii.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(c).inflate(R.layout.model_str, parent, false);
         view.setBackgroundResource(mBackground);
-        MyAdapter_mun_chis_cer_gospodarii.ViewHolder vh = new MyAdapter_mun_chis_cer_gospodarii.ViewHolder(view);
-        return vh;
+        return new ViewHolder(view);
     }
     /**
      * Our onBindViewHolder method
